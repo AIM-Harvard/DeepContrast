54aec160de9026b01fe31484a292ecdd6a3ec6b8
xmuyzz <xmuyzz@gmail.com>
2022-02-06 22:36:03 -0500

    first commit

--- src/run_prediction.py
+++ src/run_prediction.py
@@ -19,12 +19,10 @@ if __name__ == '__main__':
     """
     Prediction on IV contrast for HeadNeck and Chest CT scan;
     
-    Download models and sample data from Google Drive with the link:
-    https://drive.google.com/drive/folders/14U0pAeKXEYpQ4oD6z5NH_1BuzbGsA5vA
-    Put the models and sample data in respective folders, i.e. model_dir, data_dir.
     Args:
         proj_dir {path} -- main folder to store models, data and outputs;
         body_part {str} -- choose either 'HeadNeck' or 'Chest' to run prediction;
+    
     Return:
         Dataframe on contrast predicions on image and scan levels;
     
@@ -34,15 +32,15 @@ if __name__ == '__main__':
     body_part = 'Chest'
     
     proj_dir = '/path/to/my/folder'
-    model_dir = os.path.join(proj_dir, 'model')
-    data_dir = os.path.join(proj_dir, 'data')
-    out_dir = os.path.join(proj_dir, 'out')
+    model_dir = os.path.join(proj_dir, 'models')
+    data_dir = os.path.join(proj_dir, 'datasets')
+    out_dir = os.path.join(proj_dir, 'outputs')
     
     os.mkdir(model_dir) if not os.path.isdir(model_dir) else None
     os.mkdir(data_dir) if not os.path.isdir(data_dir) else None
     os.mkdir(out_dir) if not os.path.isdir(out_dir) else None
     
-    # choose image for registration 
+    # choose image for registration template 
     if body_part == 'HeadNeck':
         reg_temp_img = 'PMH049.nrrd'
     elif body_part == 'Chest':
